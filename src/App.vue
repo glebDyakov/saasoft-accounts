de385fc7779f3fb4543d89707b603a41126dffc7
glebdyakov <glebdyakov2000@gmail.com>
2024-07-12 09:07:52 +0300

    add app bar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
   <v-app>
+    <v-app-bar>
+        <v-toolbar-title>Учетные записи</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn icon @click="handleClick">
+          <v-icon>mdi-plus</v-icon>
+        </v-btn>
+    </v-app-bar>
     <v-main>
       <router-view />
     </v-main>
