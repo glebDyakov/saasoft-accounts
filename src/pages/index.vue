98846628cf4b0ce380b8d145264fb61b1fe0a61c
glebdyakov <glebdyakov2000@gmail.com>
2024-07-12 09:21:14 +0300

    add hint

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,19 @@
 <template>
-  <div />
+  <v-row class="mt-4 pa-2 bg-light-blue">
+    <v-col cols="">
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on, attrs }">
+          <v-icon v-bind="attrs" v-on="on">mdi-help-circle</v-icon>
+        </template>
+        <span>{{ hint }}</span>
+      </v-tooltip>
+    </v-col>
+    <v-col cols="10">
+      <span>{{ hint }}</span>
+    </v-col>
+  </v-row>
 </template>
 
 <script lang="ts" setup>
-  //
+  const hint = 'Для указания нескольких меток для одной пары логин/пароль используйте разделитель ;'
 </script>
